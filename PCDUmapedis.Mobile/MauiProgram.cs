37d3e936e9b4e9dfde3a2d17be54e85a3283876a
JOSE LUIS PINAYA BALDERRAMA <jpinayab@doc.emi.edu.bo>
2024-07-18 03:10:57 -0400

    notificacion recived

--- PCDUmapedis.Mobile/MauiProgram.cs
+++ PCDUmapedis.Mobile/MauiProgram.cs
@@ -1,4 +1,15 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Plugin.Firebase.Auth;
+using Plugin.Firebase.Bundled.Shared;
+using Microsoft.Extensions.Logging;
+using Plugin.Firebase.Crashlytics;
+using Microsoft.Maui.LifecycleEvents;
+
+#if IOS
+using Plugin.Firebase.Bundled.Platforms.iOS;
+#else
+using Plugin.Firebase.Bundled.Platforms.Android;
+#endif
+
 
 namespace PCDUmapedis.Mobile
 {
@@ -20,6 +31,32 @@ namespace PCDUmapedis.Mobile
 #endif
 
             return builder.Build();
+        }
+
+        private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+        {
+            builder.ConfigureLifecycleEvents(events =>
+            {
+#if IOS
+            events.AddiOS(iOS => iOS.FinishedLaunching((app, launchOptions) => {
+                CrossFirebase.Initialize(CreateCrossFirebaseSettings());
+                return false;
+            }));
+#else
+                events.AddAndroid(android => android.OnCreate((activity, _) =>
+                    CrossFirebase.Initialize(activity, CreateCrossFirebaseSettings())));
+                CrossFirebaseCrashlytics.Current.SetCrashlyticsCollectionEnabled(true);
+#endif
+            });
+
+            builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+            return builder;
+        }
+
+        private static CrossFirebaseSettings CreateCrossFirebaseSettings()
+        {
+            return new CrossFirebaseSettings(isAuthEnabled: true,
+            isCloudMessagingEnabled: true, isAnalyticsEnabled: true);
         }
     }
 }
